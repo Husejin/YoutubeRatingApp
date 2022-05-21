548cba8bcb70fdde5174d33ddb2ed256790e2e19
Husejin <husejinhodzic1@gmail.com>
2022-05-21 18:29:05 +0200

    quick formatting

--- MockDataGenerator/src/main/java/VideoResources.java
+++ MockDataGenerator/src/main/java/VideoResources.java
@@ -3,17 +3,15 @@ public class VideoResources {
     private static String[] videoThumbnails = new String[]{"https://img.youtube.com/vi/jAOIwNOU_I4/hqdefault.jpg", "https://img.youtube.com/vi/GtnTCjsQXB8/hqdefault.jpg", "https://img.youtube.com/vi/XB7Lzt_stNY/hqdefault.jpg", "https://img.youtube.com/vi/-1MOXM9GBFk/hqdefault.jpg", "https://img.youtube.com/vi/hapMSHVF2ks/hqdefault.jpg", "https://img.youtube.com/vi/h9Zv_TiVzmg/hqdefault.jpg", "https://img.youtube.com/vi/UjZYMI1zB9s/hqdefault.jpg", "https://img.youtube.com/vi/Bv4CqIxqTMA/hqdefault.jpg", "https://img.youtube.com/vi/jJApnB-Kf4M/hqdefault.jpg", "https://img.youtube.com/vi/4RlYKWPDPhA/hqdefault.jpg"};
     private static String[] videoTitles = new String[]{"ESL - ESPORTS PROMOTION VIDEO", "G2 Worlds 2019 Hype video", "STAY HUNGRY - Esports Motivation", "Inspire - A CS:GO motivational video by Zelli0n", "Rocket League: The Greatest Esport Of All Time (Documentary)", "The Story of Dota 2", "Free to Play", "True Sight : The International 2018 Finals", "The Story Of World of Warcraft", "WoW esports Gladiator’s Summit: Episode One"};
 
-     public static String getRandomVideoURL(int index)
-     {
-         return videoUrls[index];
-     }
+    public static String getRandomVideoURL(int index) {
+        return videoUrls[index];
+    }
 
-    public static String getRandomVideoThumbnailUrl(int index)
-    {
+    public static String getRandomVideoThumbnailUrl(int index) {
         return videoThumbnails[index];
     }
-    public static String getRandomVideoTitle(int index)
-    {
+
+    public static String getRandomVideoTitle(int index) {
         return videoTitles[index];
     }
 
